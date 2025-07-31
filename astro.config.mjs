a42fc2475df116038e7801c37634cdbfa0439c53
69Sukuna <jose.garcia@sistemas.edu.bo>
2025-07-30 21:20:10 -0400

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import db from '@astrojs/db';
-import node from '@astrojs/node';
-
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [db(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
-  output: 'server', // Necesario para las API routes y middleware
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'server',
+  adapter: vercel(),
 });
